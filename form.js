391942dbc00ef09440acd6e1186a88c80aef1e0c
shery <sheryarking5@gmail.com>
2022-07-01 17:24:10 +0500

    v2

--- form.js
+++ form.js
@@ -1,29 +1,46 @@
-function showTypedValue() {
-    var text = document.getElementById("name").value;
-    
-    // var cookiename=document.getElementById(id);
-    console.log(text);
-    localStorage.setItem("name",text)
-    // console.log(cookiename)
+// function showTypedValue() {
+//     var text = document.getElementById("name").value;
+//      console.log(text);
+//     localStorage.setItem("name",text)
+//     
  
-} 
- 
-// let name=document.getElementById('name').value;
-// console.log(name);
- 
+// } 
+
+function getid(id){
+
+     let element =document.getElementById(id);
+    element=element.id;
+    console.log(element);
+    var text = document.getElementById(element).value;
+    localStorage.setItem(id,text)
+}
+
+
 
 let ename=localStorage.getItem("name");
 document.getElementById('Sname').innerHTML=ename;
 console.log(ename);
-// let fname=localStorage.getItem("name");
-// document.getElementById('Sfname').innerHTML=fname;
-// console.log(fname);
+
+let fname=localStorage.getItem("fname");
+document.getElementById('Sfname').innerHTML=fname;
+console.log(fname);
+
+let cno=localStorage.getItem("cno");
+document.getElementById('Scontact').innerHTML=cno;
+console.log(cno);
+
+let institute=localStorage.getItem("institute");
+document.getElementById('Sinstitute').innerHTML=institute;
+console.log(institute);
+
+
+let classs=localStorage.getItem("class");
+document.getElementById('Sclass').innerHTML=classs;
+console.log(classs);
+
+let semeseter=localStorage.getItem("Semester");
+document.getElementById('Ssemester').innerHTML=semeseter;
+console.log(semeseter);
 
 // let name=localStorage.getItem("name");
-// let name=localStorage.getItem("name");
-// let name=localStorage.getItem("name");
-// let name=localStorage.getItem("name");
-// let name=localStorage.getItem("name");
-// let name=localStorage.getItem("name");
-// console.log(g)
 
